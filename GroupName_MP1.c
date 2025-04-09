8955e38beaa982136a58c805f88dd658c7e6958c
Zach Poblete <zacharypob@gmail.com>
2025-04-09 22:51:50 +0800

    Move nested Root Finding functions out
    It compiled, but only because gcc has extensions daw that support nested
    functions.

--- GroupName_MP1.c
+++ GroupName_MP1.c
@@ -11,6 +11,11 @@ void Integration(double L, double P, double E, double I);
 double Delta(double L, double P, double E, double I);
 
 void RootFinding();
+double y(double x, double P, double L, double d, double E, double I);
+double y_prime(double x, double P, double L, double d, double E, double I);
+double y_double_prime(double x, double P, double L, double d, double E, double I);
+double newton_raphson(double P, double L, double d, double E, double I, double x0);
+double regula_falsi(double P, double L, double d, double E, double I, double a, double b);
 
 int main() {
     // Uncomment for quick testing (don't have to manually input values):
@@ -238,85 +243,6 @@ double Delta(double L, double P, double E, double I) {
 //= ============================================================================
 
 void RootFinding() {
-    double y(double x, double P, double L, double d, double E, double I) {
-        return (P * (L - d)) / (4 * E * I) * (-pow(x, 2) + (1.0 / d) * pow(x, 3));
-    }
-
-    double y_prime(double x, double P, double L, double d, double E, double I) {
-        return (P * (L - d)) / (4 * E * I) * (-2 * x + (3.0 / d) * pow(x, 2));
-    }
-
-    double y_double_prime(double x, double P, double L, double d, double E, double I) {
-        return (P * (L - d)) / (4 * E * I) * (-2 + (6.0 / d) * x);
-    }
-
-    double newton_raphson(double P, double L, double d, double E, double I, double x0) {
-        double x = x0;
-        double fx, fprime_x;
-        int iter = 0;
-
-        printf("\nNewton-Raphson Method with initial guess x0 = %.6f:\n", x0);
-        printf("Iteration\t x\t\t y'(x)\t\t y''(x)\n");
-        printf("----------------------------------------------------------------\n");
-
-        do {
-            fx = y_prime(x, P, L, d, E, I);
-            fprime_x = y_double_prime(x, P, L, d, E, I);
-
-            printf("%d\t\t %.6f\t %.6e\t %.6e\n", iter, x, fx, fprime_x);
-
-            if (fabs(fprime_x) < tolerance) {
-                printf("Derivative near zero, cannot proceed with the current method. Please try again!\n");
-                return -1;
-            }
-            x -= fx / fprime_x;
-            iter++;
-        } while (fabs(fx) > tolerance && iter < 100);
-
-        printf("----------------------------------------------------------------\n");
-        printf("Converged to x = %.6f after %d iterations.\n", x, iter);
-        return x;
-    }
-
-    double regula_falsi(double P, double L, double d, double E, double I, double a, double b) {
-        double fa = y_prime(a, P, L, d, E, I);
-        double fb = y_prime(b, P, L, d, E, I);
-
-        if (fa * fb > 0) {
-            printf("Function does not change sign in the interval [%.6f, %.6f]. Please try again!\n", a, b);
-            printf("y'(a) = %.6e, y'(b) = %.6e\n", fa, fb);
-            return -1;
-        }
-
-        double c;
-        int iter = 0;
-
-        printf("\nRegula Falsi Method in interval [%.6f, %.6f]:\n", a, b);
-        printf("\nIteration\t a\t\t b\t\t c\t\t y'(c)\n");
-        printf("-------------------------------------------------------------------------------\n");
-
-        do {
-            c = (a * fb - b * fa)/ (fb - fa);
-            double fc = y_prime(c, P, L, d, E, I);
-
-            printf("%d\t\t %.6f\t %.6f\t %.6f\t %.6e\n", iter, a, b, c, fc);
-
-            if (fabs(fc) < tolerance) break;
-            if (fa * fc < 0) {
-                b = c;
-                fb = fc;
-            } else {
-                a = c;
-                fa = fc;
-            }
-            iter++;
-        } while (fabs(b - a) > tolerance && iter < 100);
-
-        printf("-------------------------------------------------------------------------------\n");
-        printf("Converged to x = %.6f after %d iterations.\n", c, iter);
-        return c;
-    }
-
     double P, L, d, E_GPa, I_mm4;
 
     printf("Enter the load P (in Newtons): ");
@@ -372,3 +298,82 @@ void RootFinding() {
 //== ===========================================================================
 //== Root Finding Helper Functions
 //== ===========================================================================
+
+double y(double x, double P, double L, double d, double E, double I) {
+    return (P * (L - d)) / (4 * E * I) * (-pow(x, 2) + (1.0 / d) * pow(x, 3));
+}
+
+double y_prime(double x, double P, double L, double d, double E, double I) {
+    return (P * (L - d)) / (4 * E * I) * (-2 * x + (3.0 / d) * pow(x, 2));
+}
+
+double y_double_prime(double x, double P, double L, double d, double E, double I) {
+    return (P * (L - d)) / (4 * E * I) * (-2 + (6.0 / d) * x);
+}
+
+double newton_raphson(double P, double L, double d, double E, double I, double x0) {
+    double x = x0;
+    double fx, fprime_x;
+    int iter = 0;
+
+    printf("\nNewton-Raphson Method with initial guess x0 = %.6f:\n", x0);
+    printf("Iteration\t x\t\t y'(x)\t\t y''(x)\n");
+    printf("----------------------------------------------------------------\n");
+
+    do {
+        fx = y_prime(x, P, L, d, E, I);
+        fprime_x = y_double_prime(x, P, L, d, E, I);
+
+        printf("%d\t\t %.6f\t %.6e\t %.6e\n", iter, x, fx, fprime_x);
+
+        if (fabs(fprime_x) < tolerance) {
+            printf("Derivative near zero, cannot proceed with the current method. Please try again!\n");
+            return -1;
+        }
+        x -= fx / fprime_x;
+        iter++;
+    } while (fabs(fx) > tolerance && iter < 100);
+
+    printf("----------------------------------------------------------------\n");
+    printf("Converged to x = %.6f after %d iterations.\n", x, iter);
+    return x;
+}
+
+double regula_falsi(double P, double L, double d, double E, double I, double a, double b) {
+    double fa = y_prime(a, P, L, d, E, I);
+    double fb = y_prime(b, P, L, d, E, I);
+
+    if (fa * fb > 0) {
+        printf("Function does not change sign in the interval [%.6f, %.6f]. Please try again!\n", a, b);
+        printf("y'(a) = %.6e, y'(b) = %.6e\n", fa, fb);
+        return -1;
+    }
+
+    double c;
+    int iter = 0;
+
+    printf("\nRegula Falsi Method in interval [%.6f, %.6f]:\n", a, b);
+    printf("\nIteration\t a\t\t b\t\t c\t\t y'(c)\n");
+    printf("-------------------------------------------------------------------------------\n");
+
+    do {
+        c = (a * fb - b * fa)/ (fb - fa);
+        double fc = y_prime(c, P, L, d, E, I);
+
+        printf("%d\t\t %.6f\t %.6f\t %.6f\t %.6e\n", iter, a, b, c, fc);
+
+        if (fabs(fc) < tolerance) break;
+        if (fa * fc < 0) {
+            b = c;
+            fb = fc;
+        } else {
+            a = c;
+            fa = fc;
+        }
+        iter++;
+    } while (fabs(b - a) > tolerance && iter < 100);
+
+    printf("-------------------------------------------------------------------------------\n");
+    printf("Converged to x = %.6f after %d iterations.\n", c, iter);
+    return c;
+}
